c5a389c1d7b2d092b73629a2da8612896768e2e9
rudyxwhite <brandonrwing98@gmail.com>
2023-05-03 09:01:43 -0500

    fixed getDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('putDb working')
   const db = await openDB("jate", 1);
@@ -22,13 +21,13 @@ export const putDb = async (content) => {
   const result = await request;
   console.log('success!', result)
 }
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.error('getDb working');
   const db = await openDB("jate", 1);
   const tx = db.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({id: 1, value: content});
+  const request = store.getAll();
   const result = await request;
   return result?.value;
 }
